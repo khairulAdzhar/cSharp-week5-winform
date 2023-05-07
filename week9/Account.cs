587c7ed15b88233353165647bf8c28c3436e753a
unknown <adzharkhai01@gmail.com>
2023-05-07 20:39:15 +0800

    first commit

---
+++ week9/Account.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace week9
+{
+    internal class Account
+    {
+        private string _FName;
+        private string _LName;
+        private decimal _Balance;
+        private decimal _Deposit;
+
+        //constructor
+        public Account() { }
+
+        public Account(decimal balance,decimal deposit)
+        {
+            _Deposit = deposit;
+            _Balance = balance + _Deposit;
+        }
+        public decimal getBalance()
+        {
+            return _Balance;
+        }
+
+        public string FirstName
+        {
+            get { return _FName; }
+            set { _FName = value; }
+        }
+        public string LastName
+        {
+            get { return _LName; }
+            set { _LName = value; }
+        }
+        public decimal BBalance { get; set; } = 5000.00m;
+
+        public decimal Deposit
+        {
+            get => _Deposit;
+
+            private set
+            {
+                if(value > 0)
+                {
+                    _Deposit = value;
+                }
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+}
---
+++ week9/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace week9
+{
+    public partial class Form1 : Form
+    {
+        Account number = new Account();
+        Account account1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        { 
+            try
+            {
+                if (comboBox1.SelectedItem.Equals("Fixed"))
+                {
+                    account1 = new Account();
+                    string bb = account1.BBalance.ToString();
+                    label7.Text = bb;
+                }
+                else
+                {
+                    account1 = new Account(decimal.Parse(textBox2.Text),decimal.Parse(textBox4.Text));
+                    label7.Text = account1.getBalance().ToString();
+                }
+                /////////////////////////
+                number.FirstName = textBox1.Text;
+                number.LastName = textBox3.Text;
+                label6.Text = number.FirstName + " " + number.LastName;
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(comboBox1.SelectedIndex == 0)
+            {
+                textBox2.Enabled = false;
+                textBox4.Enabled = false;
+
+            }
+            else
+            {
+                textBox2.Enabled = true ;
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = account1.getBalance().ToString();
+            account1 = new Account(decimal.Parse(textBox2.Text), decimal.Parse(textBox4.Text));
+            label7.Text = account1.getBalance().ToString(); 
+
+
+
+        }
+    }
+
+
+    
+
+}
